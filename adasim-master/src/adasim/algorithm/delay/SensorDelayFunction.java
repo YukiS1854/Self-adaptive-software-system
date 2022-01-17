4bb5aede2a4544e70f3f69df7f67585e89e21c6a
giantmonkeyTC <tcgiantmonkey@gmail.com>
2022-01-18 00:52:48 +0800

    test

--- adasim-master/src/adasim/algorithm/delay/SensorDelayFunction.java
+++ adasim-master/src/adasim/algorithm/delay/SensorDelayFunction.java
@@ -7,7 +7,7 @@ public class SensorDelayFunction implements TrafficDelayFunction {
     public int getDelay(int weight, int capacity, int number) {
         double prob = Math.random() * 100;
         Random generator = new Random();
-        if (prob > 95) {
+        if (prob > 99) {
             return generator.nextInt(15);
         } else
             return Math.max(weight, number - capacity + weight);
